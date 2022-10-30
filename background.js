2fa1b7bc404fd1165cb241366ab3d10aad2ec016
HJ <91491029+koreadinosaur@users.noreply.github.com>
2022-10-30 21:10:45 +0900

    Update background.js

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ const images = ["sunsetBusan.jpg","Busan.jpg","Hanok.jpg"];
 const chosenImages = images[Math.floor(Math.random()*images.length)];
 
 const bgImages = document.createElement("img");
-bgImages.src = "/todolist/img/" + chosenImages;
+bgImages.src = "/" + chosenImages;
 // `/todolist/img/${chosenImages}`; // 탬플릿 리터럴
 
 document.body.appendChild(bgImages); //prepend는 앞에 추가하는 것.
